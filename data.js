4c6e0b2c08e41bcb525c0c9b832acaae41ea8cbd
DwrUisce <bellinir@tcd.ie>
2022-09-27 14:54:49 +0100

    v1

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-gs__0286",
-      "name": "GS__0286",
+      "id": "0-yr-iard-lo",
+      "name": "Yr Iard Lo",
       "levels": [
         {
           "tileSize": 256,
@@ -24,30 +24,42 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": -0.4832735861433157,
-        "pitch": -0.1600587811557599,
-        "fov": 1.3715802068843215
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4140395332092066
       },
       "linkHotspots": [
         {
-          "yaw": 2.623731782879343,
-          "pitch": -0.12299545792679467,
+          "yaw": -0.4636089606023184,
+          "pitch": -0.46590690350992503,
           "rotation": 0,
-          "target": "1-gs__0288"
+          "target": "1-ystafelloedd-te"
+        },
+        {
+          "yaw": -0.01333122602971848,
+          "pitch": 0.16511828210392565,
+          "rotation": 0,
+          "target": "2-y-seler"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.4832735861433157,
-          "pitch": -0.1600587811557599,
-          "title": "Boiler",
-          "text": "Pre-heated water from the heat exchanger is stored and heated here"
+          "yaw": -0.11722536990479959,
+          "pitch": -0.32957197127159255,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Neges o Groeso</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"CY\">Croeso i daith rithiol o amgylch system\nadennill gwres Ystafelloedd Te Castell Penrhyn. Mae'r system hon yn gweithredu\nfel arddangosfa o'r potensial sydd i adennill gwres o'r dŵr gwastraff poeth a\ngynhyrchir mewn ceginau masnachol ac sydd fel arfer yn llifo i lawr y draen.\nBydd adennill y gwres hwn yn arbed arian trwy leihau'r defnydd a wneir o\ndanwydd a bydd hefyd yn lleihau'r allyriadau nwyon tŷ gwydr sy'n gysylltiedig â\ngwresogi dŵr. </span><span lang=\"EN-US\"><o:p></o:p></span></p>\n\n<p class=\"MsoNormal\"><span lang=\"CY\">I gychwyn ar y daith, gwnewch eich\nffordd i'r ystafelloedd te trwy glicio ar yr arwydd ^ ger y ffenestr sydd uwch\neich pen ar y chwith. </span><span lang=\"EN-US\"><o:p></o:p></span></p>\n\n<span lang=\"CY\" style=\"font-size:11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;\nmso-fareast-font-family:Calibri;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-ansi-language:CY;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Project\nyw hwn sy’n gydweithrediad rhwng Prifysgol Bangor, Coleg y Drindod Dulyn, a’r\nYmddiriedolaeth Genedlaethol. Fe’i gwnaed yn bosibl oherwydd cymorth Rhaglen\nERDF Interreg Iwerddon-Cymru 2014-2020, drwy’r Project Dŵr Uisce.</span>"
+        },
+        {
+          "yaw": 0.2893898984902208,
+          "pitch": 0.19347325700299223,
+          "title": "<span style=\"color: rgb(0, 0, 0); font-family: &quot;Times New Roman&quot;; font-size: medium;\">Castell Penrhyn, taith rithwir</span>",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: &quot;Times New Roman&quot;; font-size: medium;\">Adeiladwyd Castell Penrhyn yn yr arddull Neo-Normanaidd rhwng 1822 a 1837 gan Thomas Hopper ar gyfer George Hay Dawkins-Pennant. Adeiladwyd y castell mawr 204 ystafell ag elw Chwarel y Penrhyn, ac yn ddiweddarach ag elw planhigfeydd siwgr yn Jamaica a gâi eu gweithio yn bennaf gan gaethweision. Etifeddodd y Cyrnol Edward Gordon Douglas-Pennant y castell ac aeth ati i’w ehangu’n sylweddol ar ôl y dyrchafiad yn ei statws i fod y cyntaf o Arglwyddi’r Penrhyn. Adeiladwyd yr Iard Lo hon fel rhan o'r ehangiad hwn ym 1868 ac roedd yn bennaf yn dal coed a glo i'w defnyddio yn llety'r gweision. Rhoddwyd y Castell i'r Ymddiriedolaeth Genedlaethol i'w reoli yn 1951 ar ôl marwolaeth y 4ydd Arglwydd Penrhyn, Hugh Napier, yn 1949.</span>"
         }
       ]
     },
     {
-      "id": "1-gs__0288",
-      "name": "GS__0288",
+      "id": "1-ystafelloedd-te",
+      "name": "Ystafelloedd te",
       "levels": [
         {
           "tileSize": 256,
@@ -69,30 +81,180 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": 0.2406893577028164,
-        "pitch": -0.29859767548667193,
-        "fov": 1.3715802068843215
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -2.3715096134762135,
-          "pitch": -0.17192425500135755,
+          "yaw": 0.8123699622547065,
+          "pitch": -0.13668481081435857,
           "rotation": 0,
-          "target": "0-gs__0286"
+          "target": "0-yr-iard-lo"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.054045525839798714,
-          "pitch": -0.23904466786076028,
-          "title": "Kitchen Drain &amp; Heat Exchanger",
-          "text": "The kitchen drain was been replaced with a copper heat exchanger which pre-heats cold water. A pump moves the cold water to the boiler for storage and further heating from the heat exchanger.&nbsp;&nbsp;"
+          "yaw": -0.2791539109456025,
+          "pitch": 0.02776069004489301,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Neges ar ddŵr gwastraff y gegin&nbsp;</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"CY\">Ar ôl i chi fwynhau pryd o fwyd neu\nfyrbryd yn Ystafelloedd Te Castell Penrhyn, mae staff yr Ystafell De yn\ndefnyddio dŵr poeth ar dymheredd o tua 40-50 gradd celsius i olchi llestri budr\nac offer coginio. Yna mae'r dŵr gwastraff poeth hwn yn llifo i ddraen, trwy\ngarthffos ac i danc septig o fewn safle Castell Penrhyn, gan oeri'n raddol ar\nhyd y ffordd. Nod ein system yw adennill rhywfaint o'r gwres gwastraff hwn a\nlleihau costau gwresogi dŵr ar gyfer cegin yr Ystafell De. </span><span lang=\"EN-US\"><o:p></o:p></span></p>\n\n<span lang=\"CY\" style=\"font-size:11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;\nmso-fareast-font-family:Calibri;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-ansi-language:CY;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Parhewch\nâ’r daith drwy glicio ar yr arwydd ^ ger y ffenestr i fynd â chi yn ôl i’r Iard\nLo ac yna cliciwch ar yr ail arwydd ^ o’ch blaen i fynd â chi i ardal y seler</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-y-seler",
+      "name": "Y Seler",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
-          "yaw": -2.600034269848271,
-          "pitch": -0.41626650134880094,
-          "title": "Monitoring System",
-          "text": "A remote monitoring system that collects data about the performance of the system for future research and improvements.&nbsp;"
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.3398857991696502,
+        "pitch": -0.1213610002494292,
+        "fov": 1.4140395332092066
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9716279096562204,
+          "pitch": 0.020047173728681145,
+          "rotation": 0,
+          "target": "0-yr-iard-lo"
+        },
+        {
+          "yaw": -2.2433936574477276,
+          "pitch": 0.01507724981751224,
+          "rotation": 0,
+          "target": "3-ystafell-y-peirianwaith"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.04429294711376741,
+          "pitch": -0.1182740389229604,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Cyfnewidydd Gwres 1</span>",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-ansi-language:CY;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Mae\ndraen y gegin wedi cael ei newid am gyfnewidydd gwres copr, sydd wedi'i\ninswleiddio, sy'n cynhesu dŵr oer glân sy'n cael ei bwmpio trwy bibell gopr\nallanol sydd â phibell gopr arall y tu mewn iddi, y mae’r dŵr gwastraff yn\nllifo i lawr drwyddi. Dim ond gwres sy'n cael ei drosglwyddo trwy'r copr ac\nmae'n cynhesu'r dŵr oer sy'n cael ei gylchredeg yn ôl ac ymlaen o'r tanc storio\nyn yr ystafell reoli.</span>"
+        },
+        {
+          "yaw": -2.6504621065444187,
+          "pitch": -0.5088688432434054,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Gorsaf Fonitro</span>",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Nid oes unrhyw arddangosiad yn gyflawn heb\nddeall pa mor dda mae rhywbeth yn gweithio! Mae’r</span><span lang=\"CY\" style=\"font-size:11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;\nmso-fareast-font-family:Calibri;mso-ansi-language:CY;mso-fareast-language:EN-US;\nmso-bidi-language:AR-SA\"> system fonitro o bell hon yn casglu data am\nberfformiad y system at ddibenion ymchwil ac i wneud gwelliannau yn y dyfodol</span>"
+        },
+        {
+          "yaw": -0.20102370908610823,
+          "pitch": -0.4563103304386935,
+          "title": "<p class=\"MsoNormal\"><span lang=\"EN-US\">&nbsp;Cyfnewidydd Gwr 2<o:p></o:p></span></p>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"EN-US\">Yr un peth a Cyfnewidydd Gwr 1<o:p></o:p></span></p>"
+        }
+      ]
+    },
+    {
+      "id": "3-ystafell-y-peirianwaith",
+      "name": "Ystafell y Peirianwaith",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6631746057485888,
+          "pitch": -0.05518392518955473,
+          "rotation": 0,
+          "target": "2-y-seler"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1178219383011943,
+          "pitch": -0.20871536165582327,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Pwmp Cyflenwi</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"CY\">Mae'r pwmp hwn yn cyflenwi dŵr wedi'i\ngynhesu i'r Uned Gymysgu.</span><span lang=\"EN-US\"><o:p></o:p></span></p>"
+        },
+        {
+          "yaw": -0.29705258867315365,
+          "pitch": -0.6220083283841689,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Tanc Storio Dŵr Poeth</span>",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Caiff dŵr </span><span lang=\"CY\" style=\"font-size:\n11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:\nCalibri;mso-ansi-language:CY;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">ei\nbwmpio allan o'r tanc hwn i'r cyfnewidydd gwres. Mae hefyd yn cyflenwi'r uned\ngymysgu â dŵr wedi'i gynhesu i gyflenwi'r ceginau.</span>"
+        },
+        {
+          "yaw": -0.015156345767099566,
+          "pitch": -0.16966971678927045,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Pwmp Ailgylchredeg</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"CY\">Mae'r pwmp hwn yn trosglwyddo dŵr oer i\nmewn a dŵr cynnes allan o'r cyfnewidydd gwres.</span><span lang=\"EN-US\"><o:p></o:p></span></p>"
+        },
+        {
+          "yaw": 0.4102206083103308,
+          "pitch": -0.06302099605657574,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Pibellau</span>",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Mae'r pibellau hyn yn cludo dŵr oer i'r cyfnewidydd\ngwres yn y seler, a dŵr poeth ohono. Allwch chi eu dilyn yr holl ffordd i'r\ncyfnewidydd gwres!</span>"
+        },
+        {
+          "yaw": 0.413809356716369,
+          "pitch": 0.10561193822901949,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Pibellau</span>",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Mae'r pibellau hyn yn cludo dŵr oer i'r cyfnewidydd\ngwres yn y seler, a dŵr poeth ohono. Allwch chi eu dilyn yr holl ffordd i'r\ncyfnewidydd gwres!</span>"
+        },
+        {
+          "yaw": -3.12788188690919,
+          "pitch": -0.31773235509434805,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Diolch!</span>",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Ar ôl i chi orffen edrych o gwmpas yr ystafell\nhon, dyma ddiwedd ein taith. Diolch i chi am eich diddordeb yn ein project!</span>"
+        },
+        {
+          "yaw": 1.2071016706062956,
+          "pitch": -0.5885311797324118,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.017721660877113,
+          "pitch": -0.6408192081640394,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Uned Gymysgu</span>",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Mae'r uned hon yn cymysgu'r dŵr sy'n cael ei\ngynhesu gan y cyfnewidydd gwres o'r tanc storio gyda dŵr sy'n cael ei gynhesu\ngan foeleri'r Castell i gyflenwi dŵr poeth ar dymheredd addas i geginau'r\nYstafell De</span>"
+        },
+        {
+          "yaw": -0.34158454260102467,
+          "pitch": 0.19431769856750059,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Chwiliedydd Dŵr Poeth</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"CY\">Synhwyrydd sy'n mesur tymheredd y dŵr\npoeth sy'n cael ei gofnodi yn yr orsaf fonitro.&nbsp;</span><span lang=\"EN-US\"><o:p></o:p></span></p>"
         }
       ]
     }
@@ -101,7 +263,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
-    "viewControlButtons": true
+    "fullscreenButton": true,
+    "viewControlButtons": false
   }
 };
